49850aad02a9ca9bb8ae0ac33c0455266493012d
Echandler123 <elijah.chandler@menloschool.org>
2025-01-12 21:55:25 -0800

    update

--- src/Boggle.java
+++ src/Boggle.java
@@ -12,6 +12,17 @@ public class Boggle {
         // TODO: Complete the function findWords(). Add all words that are found both on the board
         //  and in the dictionary.
         boolean [][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < dictionary.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                for (int k = 0; k < board[j].length; k++) {
+                    if (dictionary[i].charAt(0) == board[j][k]) {
+                        visited[j][k] = true;
+                        dfs(j, k, dictionary[i],visited);
+                    }
+                }
+            }
+        }
+        // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
@@ -28,3 +39,4 @@ public class Boggle {
         }
     }
 }
+
