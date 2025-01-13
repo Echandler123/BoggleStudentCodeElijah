04208332afb12125fafd47fefb8668ac314209b2
Echandler123 <elijah.chandler@menloschool.org>
2025-01-12 20:37:50 -0800

    update

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Boggle {
+
 
     public static String[] findWords(char[][] board, String[] dictionary) {
 
-        ArrayList<String> goodWords = new ArrayList<String>();
 
+        ArrayList<String> goodWords = new ArrayList<String>();
         // TODO: Complete the function findWords(). Add all words that are found both on the board
         //  and in the dictionary.
-        // Convert the list into a sorted array of strings, then return the array.
+        boolean [][] visited = new boolean[board.length][board[0].length];
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
     }
-
+    public static void dfs(int row, int col, String word,boolean[][] visited){
+//        if we have been here before, return
+//        if this word is not a valid prefix, return
+//                mark this square as visited
+//        recurse up, down, left, right with updated word
+//        mark this square as not visited
+        if(visited[row][col] == true){
+            return;
+        }
+    }
 }
